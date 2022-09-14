a858390378160dcbd3a78322d57423d1ec54b7aa
FrancescMC <francescmc93@gmail.com>
2022-09-14 09:58:08 +0200

    added some changes

--- src/movies.js
+++ src/movies.js
@@ -81,7 +81,8 @@ const orderAlphabetically = (moviesArray) => {
 };
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+// function turnHoursToMinutes(moviesArray) {}
+const turnHoursToMinutes = (moviesArray) => {};
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
